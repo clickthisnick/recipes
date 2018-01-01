1cdd671889f41bd17a3b8aa4d9fc3d19236c3df4
Nick Smith <clickthisnick@users.noreply.github.com>
2017-12-31 23:39:19 -0500

    Chore - add missing break maker instructions (#7)

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-// import { Bread } from './recipes/bread';
+import { Bread } from './recipes/bread';
 // import { PadThaiStirFry } from './recipes/padthaistiryfry';
 // import { GarlicPasta } from './recipes/garlicpasta';
 // import { SpaghettiZuchinniPasta } from './recipes/spaghettiZuchinniPasta';
 // import { PadThaiStirFry } from './recipes/guacamole';
 // import { Dummy } from './recipes/dummy';
-import { SpicyLeekMushrooms } from './recipes/spicyleekmushrooms';
+// import { SpicyLeekMushrooms } from './recipes/spicyleekmushrooms';
 // import { AirFrozenChicken } from './recipes/airFrozenChicken';
 
-const recipe = new SpicyLeekMushrooms();
+const recipe = new Bread();
 recipe.generateRecipe();
