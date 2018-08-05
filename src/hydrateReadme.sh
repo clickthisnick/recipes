02fb3d4d406fbed0ebe56b5e1ec9b67d7e58f376
MyComputer <mycomputer@MyComputer.lan>
2018-08-05 12:57:21 -0400

    Chore - fix bash issue which overwrote readme

--- src/hydrateReadme.sh
+++ src/hydrateReadme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo '## [Meat Temperature](https://rawgit.com/clickthisnick/recipes/master/meatTemp.html)' > README.MD
-echo '## [Water Intake](https://rawgit.com/clickthisnick/recipes/master/waterIntake.html)' > README.MD
-echo '## [Grocery List](https://rawgit.com/clickthisnick/recipes/master/groceryList.html)' > README.MD
+echo '## [Water Intake](https://rawgit.com/clickthisnick/recipes/master/waterIntake.html)' >> README.MD
+echo '## [Grocery List](https://rawgit.com/clickthisnick/recipes/master/groceryList.html)' >> README.MD
 echo '# Recipes List' >> README.MD
