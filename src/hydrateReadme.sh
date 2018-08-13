08d8e3b6b0c3e640fd80d5600b1457a34461396e
MyComputer <mycomputer@MyComputer.lan>
2018-08-12 21:30:58 -0400

    Chore - add expiration date calculator html readme

--- src/hydrateReadme.sh
+++ src/hydrateReadme.sh
@@ -3,4 +3,5 @@
 echo '## [Meat Temperature](https://rawgit.com/clickthisnick/recipes/master/meatTemp.html)' > README.MD
 echo '## [Water Intake](https://rawgit.com/clickthisnick/recipes/master/waterIntake.html)' >> README.MD
 echo '## [Grocery List](https://rawgit.com/clickthisnick/recipes/master/groceryList.html)' >> README.MD
+echo '## [Expiration Calculator](https://rawgit.com/clickthisnick/recipes/master/expirationCalculator.html)' >> README.MD
 echo '# Recipes List' >> README.MD
