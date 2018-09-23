53ddaf9f6ed861da363559933457df65e592aec2
Nick Smith <admin@clickthisnick.com>
2018-09-23 16:42:19 -0400

    Another readme file

--- src/hydrateReadme.sh
+++ src/hydrateReadme.sh
@@ -4,4 +4,5 @@ echo '## [Meat Temperature](https://rawgit.com/clickthisnick/recipes/master/meat
 echo '## [Water Intake](https://rawgit.com/clickthisnick/recipes/master/waterIntake.html)' >> README.MD
 echo '## [Grocery List](https://rawgit.com/clickthisnick/recipes/master/groceryList.html)' >> README.MD
 echo '## [Expiration Calculator](https://rawgit.com/clickthisnick/recipes/master/expirationCalculator.html)' >> README.MD
+echo '## [Bubble Bath](https://rawgit.com/clickthisnick/recipes/master/bubblebath.html)' >> README.MD
 echo '# Recipes List' >> README.MD
