75a3166dfd6295c262fe7acbd3db39e1b3307b87
Nick Smith <clickthisnick@users.noreply.github.com>
2018-10-18 07:41:38 -0400

    Chore - fix https issue with github page (#18)

--- src/class/recipe.ts
+++ src/class/recipe.ts
@@ -229,7 +229,7 @@ export class Recipe {
               Readme.groups[this.recipeGroup] = [];
           }
 
-          Readme.groups[this.recipeGroup].push(`## [${this.recipeName}](https://clickthisnick.com/recipes/dist/${this.recipeName}.html)\n\n`);
+          Readme.groups[this.recipeGroup].push(`## [${this.recipeName}](https://www.clickthisnick.com/recipes/dist/${this.recipeName}.html)\n\n`);
 
           // TODO This has an error
           fs.writeFile(`${process.cwd()}/dist/${this.recipeName}.html`, this.recipeHtml, (err) => {
