dd3144533ee45756f5073330b42e239dd4b10d07
clickthisnick <admin@clickthisnick.com>
2019-09-30 21:01:06 -0400

    Updated recipes

--- src/recipes/beyondBurger.ts
+++ src/recipes/beyondBurger.ts
@@ -1,8 +1,8 @@
 import { Recipe } from '../class/recipe';
 import { Items as i } from '../constants/items';
 import { Categories as c } from '../constants/categories';
-import { Timer } from '../class/timer';
 
+// Serves 1
 export class MealRecipe extends Recipe {
     constructor() {
         super();
@@ -17,10 +17,11 @@ export class MealRecipe extends Recipe {
     public generateRecipe() {
         this.prep();
         this.addSteps([
-            [Timer.set(3, 'm', 'Preheat pan on heat 6')],
-            ['Put', this.get(i.beyondBurger()), 'in pan'],
-            [Timer.set(4, 'm', 'Cook. No oil.')],
-            [Timer.set(4, 'm', 'Flip beyond burger')],
+            ['Put', this.get(i.beyondBurger()), 'on pan on heat 5'],
+            ['Wait 3 min/Flip'],
+            ['Wait 3 min/Flip'],
+            ['Wait 5 min/Flip'],
+            ['Wait 5 min/Flip'],
             ['Use', this.get(i.briocheHamburgerBun()), 'for buns'],
         ]);
         this.printRecipe();
