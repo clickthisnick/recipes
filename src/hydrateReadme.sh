6b5e3e33a9df73ce25acd1f89045b87c518ccefb
clickthisnick <admin@clickthisnick.com>
2020-02-08 18:54:28 -0500

    add frozen meat

--- src/hydrateReadme.sh
+++ src/hydrateReadme.sh
@@ -4,4 +4,5 @@ echo '## [Meat Temperature](https://www.clickthisnick.com/recipes/meatTemp.html)
 echo '## [Water Intake](https://www.clickthisnick.com/recipes/waterIntake.html)' >> README.MD
 echo '## [Grocery List](https://www.clickthisnick.com/recipes/groceryList.html)' >> README.MD
 echo '## [Expiration Calculator](https://www.clickthisnick.com/recipes/expirationCalculator.html)' >> README.MD
+echo '## [Frozen Meat Portions](https://www.clickthisnick.com/recipes/frozenMeat.html)' >> README.MD
 echo '# Recipes List' >> README.MD
