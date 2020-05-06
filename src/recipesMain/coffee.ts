3a506074d9427ca1c148b9cf33e98a61536a1438
mycomputer <mycomputer@mycomputers-MacBook-Pro.local>
2020-05-05 23:31:15 -0400

    update recipes

--- src/recipesMain/coffee.ts
+++ src/recipesMain/coffee.ts
@@ -3,6 +3,7 @@ import { Items as i } from '../constants/items';
 import { Categories as c } from '../constants/categories';
 import { Units as u } from '../constants/units';
 import { Timer } from '../class/timer';
+import { Async } from '../class/async';
 
 export class MealRecipe extends RecipeContainer {
     constructor() {
@@ -29,27 +30,18 @@ class Coffee extends Recipe {
         ]);
         this.addSteps([
             ['Fill up teapot with water'],
-            ['Turn stove to "HIGH" (after 9)'],
-            ['Wait until whistles'],
-            ['Put', i.coffeeGrounds(), 'into french press bag'],
-            ['Put french press bag into french press'],
+            [Timer.set(10, 'm', 'Turn stove to "HIGH" (after 9) and wait for whistle')],
+            [Async.step, 'Put', i.coffeeGrounds(), 'into french press bag'],
+            [Async.step, 'Put french press bag into french press'],
             ['Once teapot whistles - pour water into french press until water level reaches silver chrome part'],
             [Timer.set(4, 'm', 'Put top onto french press')],
             ['Push french press top down'],
             ['Put 1 scoop collagen into a cup'],
-
-            // Non iced
-            ['None Iced coffee'],
+            ['Pour 2 inches of coffee'],
+            ['Stir collagen'],
+            ['If iced coffee - Fill cup with ice'],
             ['Pour coffee into cup, 1 inch from top'],
             ['Top off with creamer and stir'],
-
-            // Iced Coffee
-            ['Iced coffee'],
-            ['Pour 2 inch coffee into cup'],
-            ['Stir to dissolve collagen'],
-            ['Fill cup with ice'],
-            ['Pour coffee into cup, 1 inch from top'],
-            ['Top off with creamer and sitr'],
         ]);
     }
 }
