9f8e4b0252067b5a0063a4377c1ba0e1786c0f58
mycomputer <mycomputer@mycomputers-MacBook-Pro.local>
2020-08-09 12:08:50 -0400

    add pull slicer table

--- src/hydrateReadme.sh
+++ src/hydrateReadme.sh
@@ -4,5 +4,6 @@ echo '## [Meat Temperature](https://www.clickthisnick.com/recipes/meatTemp.html)
 echo '## [Water Intake](https://www.clickthisnick.com/recipes/waterIntake.html)' >> README.MD
 echo '## [Grocery List](https://www.clickthisnick.com/recipes/groceryList.html)' >> README.MD
 echo '## [Expiration Calculator](https://www.clickthisnick.com/recipes/expirationCalculator.html)' >> README.MD
+echo '## [Pull Cord Slicer Amount](https://www.clickthisnick.com/recipes/cordSlicer.html)' >> README.MD
 echo '## [Frozen Food Portions](https://www.clickthisnick.com/recipes/frozenFood.html)' >> README.MD
 echo '# Recipes List' >> README.MD
