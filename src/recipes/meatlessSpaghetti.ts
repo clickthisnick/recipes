27e133e54d8359c6a5449a5a078a0b4fdf2107fa
Nick Smith <clickthisnick@users.noreply.github.com>
2025-11-16 16:30:53 -0500

    Update meatlessSpaghetti.ts

--- src/recipes/meatlessSpaghetti.ts
+++ src/recipes/meatlessSpaghetti.ts
@@ -9,6 +9,7 @@ export class MealRecipe extends RecipeContainer {
         super();
         this.variations = [
             MeatlessSpaghetti,
+            MeatlessSpaghettiNoMushrooms,
         ]
     }
 }
@@ -44,6 +45,66 @@ class MeatlessSpaghetti extends Recipe {
                 ]),
                 e.pan().cook(9, 'm', PAN_TEMPERATURE),
                 Timer.end(),
+                e.pan().add([
+                    i.walnut(.33, u.cup),
+                ]),
+                e.pan().cook(2, 'm', PAN_TEMPERATURE),
+                Timer.end(),
+                e.pan().add([
+                    i.wholeFennelSeeds(1.5, u.tsp),
+                    i.smokedPaprika(.5, u.tsp),
+                    i.sweetPaprika(.5, u.tsp),
+                    i.redPepperFlakes(.5, u.tsp),
+                    i.garlicPowder(.5, u.tsp),
+                    i.onionPowder(.5, u.tsp),
+                    i.driedOregano(.5, u.tsp),
+                    i.driedThyme(.25, u.tsp),
+                    i.blackPepper(.25, u.tsp),
+                    i.kosherSalt(.5, u.tsp),
+                ]),
+                e.pan().cook(30, 's', PAN_TEMPERATURE),
+                Timer.end(),
+                e.pan().add([
+                    i.soySauce(2, u.tsp),
+                    i.nutritionalYeast(1, u.tbsp),
+                    i.balsamicVinegar(.5, u.tsp),
+                    i.spaghettiSauce(1, u.unit),
+                ]),
+            Timer.end(),
+            e.pan().cook(8, 'm', PAN_TEMPERATURE),
+            Timer.end(),
+            e.pan().add([
+                i.lentilSpaghetti(),
+            ]),
+        ];
+    }
+}
+
+
+class MeatlessSpaghettiNoMushrooms extends Recipe {
+    constructor() {
+        super();
+        this.steps = [
+            e.pan().add([
+                i.oliveOil(1, u.tbsp),
+            ]),
+            e.pan().cook(1, 'm', PAN_TEMPERATURE),
+            Timer.end(),
+            e.pan().add([
+                i.whiteOnion(1, u.unit),
+            ]),
+            e.pan().cook(5, 'm', PAN_TEMPERATURE),
+            Timer.end(),
+            e.pan().add([
+                i.garlicClove(3, u.clove),
+            ]),
+            e.pan().cook(1, 'm', PAN_TEMPERATURE),
+            Timer.end(),
+            e.pot().add([
+                i.lentilSpaghetti(),
+                i.water(16, u.ounce),
+            ]),
+            e.pot().cookWithLid(3, 'm', 9),
                 e.pan().add([
                     i.walnut(.33, u.cup),
                 ]),
