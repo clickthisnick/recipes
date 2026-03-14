35c79063bb6fb32e53d15ba7afac0bd33977ca71
Nick Smith <admin@clickthisnick.com>
2026-03-14 19:45:04 -0400

    update breaded mushroom recipe

--- src/recipes/breadedMushrooms.ts
+++ src/recipes/breadedMushrooms.ts
@@ -3,6 +3,7 @@ import { Items as i } from '../constants/items';
 import { Equipment as e } from '../class/equipment';
 import { Units as u } from '../constants/units';
 import { Text as text} from '../class/text';
+import { Timer } from '../class/timer';
 
 export class MealRecipe extends RecipeContainer {
     constructor() {
@@ -18,19 +19,25 @@ class BreadedMushrooms extends Recipe {
         super();
         this.steps = [
             text.set(["Preheat oven to 450"]),
-            text.set(["Cook panko for 5 minutes"]),
+            Timer.set(5, 'm', 'Cook panko'),
+            Timer.end(),
             e.bowl().add([
                 i.cassavaFlour(.5, u.cup),
                 i.cornstarch(2, u.tbsp),
-                i.water(4, u.ounce),
+                i.water(7, u.ounce),
                 i.salt(.5, u.tsp),
                 i.garlicPowder(.5, u.tsp),
             ]),
             text.set(["Slice 3–4 king oyster mushrooms"]),
             text.set(["Dip mushrooms into mixture, let drain, then dip into panko"]),
             text.set(['Spray', i.avocadoOil(), 'on panko']),
-            text.set(['Cook for 25 minutes, flipping halfway']),
-            text.set(['Broil for 1 minute']),
+            Timer.set(12, 'm', 'Cook'),
+            Timer.end(),
+            text.set(['Flip']),
+            Timer.set(13, 'm', 'Cook'),
+            Timer.end(),
+            Timer.set(1, 'm', 'Broil'),
+            Timer.end(),
         ];
     }
 }
