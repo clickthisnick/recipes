cebf11b08948a7c57c2d0264bcbcb95119088052
Nick Smith <admin@clickthisnick.com>
2026-04-24 07:46:42 -0400

    add morning streetch

---
+++ src/recipes/morningStretching.ts
@@ -0,0 +1,45 @@
+import { Recipe, RecipeContainer } from '../class/recipe';
+import { Items as i } from '../constants/items';
+import { Equipment as e } from '../class/equipment';
+import { Units as u } from '../constants/units';
+import { Text as text} from '../class/text';
+import { Timer } from '../class/timer';
+
+export class MealRecipe extends RecipeContainer {
+    constructor() {
+        super();
+        this.variations = [
+            MorningStretch
+        ]
+    }
+}
+
+const duration = 60
+
+class MorningStretch extends Recipe {
+    constructor() {
+        super();
+        this.steps = [
+            Timer.set(duration, 's', '2 Leg Down'),
+            Timer.end(),
+            Timer.set(duration, 's', 'standing quad leg to back stretch right'),
+            Timer.end(),
+            Timer.set(duration, 's', 'standing quad leg to back stretch left'),
+            Timer.end(),
+            Timer.set(duration, 's', 'Butterfly'),
+            Timer.end(),
+            Timer.set(duration, 's', 'Back Twist Left'),
+            Timer.end(),
+            Timer.set(duration, 's', 'Back Twist Right'),
+            Timer.end(),
+            Timer.set(duration, 's', 'Butterfly one leg right straight'),
+            Timer.end(),
+            Timer.set(duration, 's', 'Butterfly one leg left straight'),
+            Timer.end(),
+            Timer.set(duration, 's', 'Cobra'),
+            Timer.end(),
+            Timer.set(duration, 's', 'Two Leg Down'),
+            Timer.end(),
+        ];
+    }
+}
